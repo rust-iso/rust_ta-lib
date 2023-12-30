9591953a23b45abba680d2b5db18d9d8fef85477
Jiang Yunhan <ana.jiangvz@gmail.com>
2023-12-30 21:31:16 +0800

    wrap ULTOSC - Ultimate Oscillator
    e.g., real = ULTOSC(high, low, close, timeperiod1=7, timeperiod2=14, timeperiod3=28)
    https://ta-lib.github.io/ta-lib-python/func_groups/momentum_indicators.html

--- src/wrapper.rs
+++ src/wrapper.rs
@@ -2438,7 +2438,66 @@ pub fn tsf() {}
 pub fn s_tsf() {}
 pub fn typprice() {}
 pub fn s_typprice() {}
-pub fn ultosc() {}
+
+///
+/// TA_ULTOSC - Ultimate Oscillator
+///
+/// `Input`:  high, low, close, timeperiod1, timeperiod2, timeperiod3
+///
+/// `Output`: (1st, 2nd)
+///
+///    1st: output vector(f64)
+///
+///    2nd: the first index of inputs corresponding to an valid output value
+///
+pub fn ultosc(
+    high: &Vec<f64>,
+    low: &Vec<f64>,
+    close: &Vec<f64>,
+    timeperiod1: i32,
+    timeperiod2: i32,
+    timeperiod3: i32,
+) -> (Vec<f64>, crate::TA_Integer) {
+    let clen = close.len();
+    if clen.ne(&high.len()) || clen.ne(&low.len()) {
+        panic!("The length of input vectors are not equal, please double check the size of each.");
+    }
+
+    let mut out: Vec<f64> = Vec::with_capacity(clen);
+    let mut out_begin: crate::TA_Integer = 0;
+    let mut out_size: crate::TA_Integer = 0;
+
+    unsafe {
+        crate::TA_Initialize();
+        let ret_code = crate::TA_ULTOSC(
+            0,                // the first index of the input vector to use
+            clen as i32 - 1,  // the last index of the input vector to use
+            high.as_ptr(),    // pointer to the high vector
+            low.as_ptr(),     // pointer to the low vector
+            close.as_ptr(),   // pointer to the close vector
+            timeperiod1,      // time period1
+            timeperiod2,      // time period2
+            timeperiod3,      // time period3
+            &mut out_begin,   // set to index of the first close to have an valid output value
+            &mut out_size,    // set to number of values computed
+            out.as_mut_ptr(), // pointer to the first element of the output vector
+        );
+
+        match ret_code {
+            // Indicator was computed correctly, since the vector was filled by TA-lib C library,
+            // Rust doesn't know what is the new length of the vector, so we set it manually
+            // to the number of values returned by the TA_ATR call
+            crate::TA_RetCode_TA_SUCCESS => {
+                out.set_len(out_size as usize);
+            }
+            // An error occured
+            _ => panic!("Could not compute indicator, err: {:?}", ret_code),
+        }
+        crate::TA_Shutdown();
+    }
+
+    (out, out_begin)
+}
 pub fn s_ultosc() {}
 pub fn var() {}
 pub fn s_var() {}
